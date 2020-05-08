0986748c672a49ee420bf585b6dd6a8bed61a9c3
benfc1993 <41189326+benfc1993@users.noreply.github.com>
2020-05-08 01:45:36 +0100

    change lobby js

--- views/lobby.js
+++ views/lobby.js
@@ -1,23 +1,23 @@
-socket = io.connect("https://davidcollins.eu/:3000");
+socket = io.connect('https://localhost:3000');
 
-socket.on("404-error", () => {
-	window.location.href = "/";
+socket.on('404-error', () => {
+	window.location.href = '/';
 });
 
-const lobbyTitle = document.getElementById("lobby-title");
-const usersList = document.getElementById("users-list");
-const userData = JSON.parse(localStorage.getItem("game_data"));
-const cardFields = document.getElementsByClassName("card-field");
+const lobbyTitle = document.getElementById('lobby-title');
+const usersList = document.getElementById('users-list');
+const userData = JSON.parse(localStorage.getItem('game_data'));
+const cardFields = document.getElementsByClassName('card-field');
 var localUsers = [];
 var deck = [];
 
-socket.emit("joinLobby", userData);
+socket.emit('joinLobby', userData);
 
 lobbyTitle.innerText = `Lobby for ${userData.roomCode}`;
 
-socket.on("updateLobby", (users) => {
-	console.log("update");
-	usersList.innerHTML = "";
+socket.on('updateLobby', (users) => {
+	console.log('update');
+	usersList.innerHTML = '';
 	users.forEach((user, index) => {
 		if (localUsers.indexOf(user) == -1) {
 			localUsers.push(user);
@@ -30,9 +30,9 @@ socket.on("updateLobby", (users) => {
 
 startGame = () => {
 	for (let field of cardFields) {
-		let qty = field.querySelector(".card-field__qty").value;
+		let qty = field.querySelector('.card-field__qty').value;
 		for (let i = 0; i <= qty; i++) {
-			deck.push(field.getAttribute("data-type"));
+			deck.push(field.getAttribute('data-type'));
 		}
 	}
 	deck = deck.sort(shuffle);
@@ -40,9 +40,9 @@ startGame = () => {
 		roomCode: userData.roomCode,
 		deck: deck,
 	};
-	socket.emit("startGame", data);
+	socket.emit('startGame', data);
 };
 shuffle = (a, b) => 0.5 - Math.random();
-socket.on("loadGame", () => {
-	window.location.href = "/game";
+socket.on('loadGame', () => {
+	window.location.href = '/game';
 });
