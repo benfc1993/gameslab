29b69be7df1929b1d0d4bfb5811df50b4db0667f
benfc1993 <41189326+benfc1993@users.noreply.github.com>
2020-05-08 15:12:33 +0100

    change db connection

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ var socket = require('socket.io');
 var io = socket(server);
 // var sql = require("./db.js");
 
-const { Client } = require('pg');
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
-const sql = new Client({
-	connectionString: process.env.DATABASE_URL,
-	ssl: true,
+const sql = new Pool({
+	connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+	ssl: isProduction,
 });
 sql.connect();
 
